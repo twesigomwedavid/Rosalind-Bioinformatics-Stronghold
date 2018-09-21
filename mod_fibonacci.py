5197881620ab37d579955e185c905e8ef7a8c755
twesigomwedavid <38982270+twesigomwedavid@users.noreply.github.com>
2018-09-21 12:37:57 +0300

    Update mod_fibonacci.py

--- mod_fibonacci.py
+++ mod_fibonacci.py
@@ -3,7 +3,6 @@ In modified version of fibonacci rabbit problem,
 each reproduction-age pair produces k-pairs to add to the litter
 instead of only 1 pair. The rabbits are still immortal.
 Function returns total number of rabbits after n months.
-
 '''
 def mod_fib(n, k):
     a, b = 1, 1
